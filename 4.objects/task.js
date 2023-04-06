efe766c8e1b2ec7d5bd5694a9cc61ba8828466e5
Lily <l_chudinova@mail.ru>
2023-04-07 01:07:39 +0300

    obj task added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.marks = [];
 }
+const student1 = new Student("Василиса", "женский", 19);
+const student2 = new Student("Артем", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks === undefined) {
+    console.log("Студент отчислен");
+  } else {
+    return this.marks.push(...marksToAdd);
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function (...marks) {
+  let sum = this.marks.reduce((acc, marks) => acc + marks);
+  let avg = sum/this.marks.length;
+  return avg;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
